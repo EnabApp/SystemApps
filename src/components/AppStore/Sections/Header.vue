affc31f70f09d8b40608edc059c1c65b3aaa1831
Montazer Malik <mntthr1122@gmail.com>
2022-08-07 00:37:09 +0300

    Header dark light mode

--- src/components/AppStore/Sections/Header.vue
+++ src/components/AppStore/Sections/Header.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="flex justify-between grid-cols-3">
   <div class="flex place-items-center">
-    <UiInput placeholder="البحث عن تطبيق" icon="i-akar-icons:search" w="[484px]" />
+    <UiInput placeholder="البحث عن تطبيق" bg="primary dark:primaryOp" rounded="lg" icon="i-akar-icons:search" w="[484px]" />
   </div>
   <div class="flex place-items-center">
-    <span text="w-90" m-l="6">الخدمات</span>
-    <span text="w-90" m-l="6">التطبيقات</span>
-    <span text="w-90" m-l="6">الحزم</span>
+    <span text="primaryOp dark:primary" m-l="6">الخدمات</span>
+    <span text="primaryOp dark:primary" m-l="6">التطبيقات</span>
+    <span text="primaryOp dark:primary" m-l="6">الحزم</span>
   </div>
   <div class="flex place-items-center">
-    <div class="i-ri:copper-coin-fill" text="w-90" w="[32px]" h="[32px]"></div>
-    <span text="w-90" m-r-3>1999</span>
+    <div class="i-ri:copper-coin-fill" text="primaryOp dark:primary" w="[32px]" h="[32px]"></div>
+    <span text="primaryOp dark:primary" m-r-3>1999</span>
   </div>
 </div>
 </template>
