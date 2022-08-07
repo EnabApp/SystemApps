9c2a5a8f6b09fbb306a7f4180f65128601d975ca
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-07 12:48:30 +0300

    Update AppStore.vue

--- src/components/AppStore.vue
+++ src/components/AppStore.vue
@@ -2,7 +2,7 @@
   <!-- Application -->
   <Transition>
     <UiDesktopWindow v-if="app.running" v-show="!app.minimized" :app="app">
-      <div class="flex bg-b-90">
+      <div class="flex bg-b-90" h="full">
         <!-- sidebare -->
         <div class="flex place-content-center" w="[96px]" m-t="[36px]">
           <AppStoreSectionsSidebar :app="app" w="[64px]" />
