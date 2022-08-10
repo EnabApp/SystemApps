c705d15a678c47459ac260dc827eec7e6ad7b65d
Montazer Malik <mntthr1122@gmail.com>
2022-08-10 07:55:25 +0300

    يي

--- src/runtime/registerer.ts
+++ src/runtime/registerer.ts
@@ -2,6 +2,7 @@ import { defineNuxtPlugin } from '#app'
 import { useStoreApps } from '~/composables/useStore/useStoreApps'
 import config from './config.json'
 
+
 export default defineNuxtPlugin((nuxtApp) => {
     // Registering App
     const appStore = useStoreApps()
