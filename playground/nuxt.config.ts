729f9e481b491176c9439518112976d063f78bc5
Hasan Alkarnosy <gruceing@gmail.com>
2022-09-10 13:10:17 +0300

    commit from core

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
         MyModule,
         '@enab/utilities',
         '@enab/core',
-        '@nuxt/image',
+        '@nuxt/image-edge',
         '@nuxtjs/supabase',
         '@pinia/nuxt',
         '@vueuse/nuxt',
