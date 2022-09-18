1ac6d0f784ae75929d3ab59c58a44ec86d86776c
Montazer Malik <64214350+ste7een@users.noreply.github.com>
2022-09-18 14:09:45 +0300

    Responsive

--- src/runtime/components/AppStore/Apps/CardInfo.vue
+++ src/runtime/components/AppStore/Apps/CardInfo.vue
@@ -2,121 +2,56 @@
   <div>
     <!-- Back Button -->
     <div v-if="!breakpoint.twoXs">
-      <div
-        @click="appStore.back()"
-        cursor="pointer"
-        flex="~ gap-2"
-        justify="center"
-        m-t="30px"
-        w="122px"
-        h="41px"
-      >
+      <div @click="appStore.back()" cursor="pointer" flex="~ gap-2" justify="center" m-t="30px" w="122px" h="41px">
         <AppStoreIconBack w="5" h="5" text="primaryOp dark:primary" />
         <span text="xl primaryOp dark:primary"> الرجوع </span>
       </div>
     </div>
-    <span text="primary 3xl">{{ breakpoint.size }}</span>
-    <div
-      :class="
-        !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
-          ? 'flex'
-          : 'flex flex-col'
-      "
-    >
+    <div :class="
+      !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
+        ? 'flex'
+        : 'flex flex-col'
+    ">
       <!-- Main Section -->
-      <div
-        transition="all duration-400 ease-in-out delay-75"
-        :class="
-          !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
-            ? 'w-60%'
-            : 'w-95%'
-        "
-      >
+      <div transition="all duration-400 ease-in-out delay-75" :class="
+        !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
+          ? 'w-60%'
+          : 'w-95%'
+      ">
         <!-- Icom ,Name and install -->
         <div flex="~ gap-3" items="center" w="full" h="auto">
           <!-- Icon -->
-          <component
-            text="primaryOp dark:primary"
-            transition="all duration-400 ease-in-out delay-75"
-            w="180px"
-            h="180px"
-            :is="`${appStore.selectedApp.name}Icon`"
-          ></component>
+          <component text="primaryOp dark:primary" transition="all duration-400 ease-in-out delay-75" w="180px" h="180px" :is="`${appStore.selectedApp.name}Icon`"></component>
           <!-- Name and install -->
           <div h="full">
             <span text="primaryOp dark:primary 3xl" font="semibold" m-b="3%">
               {{ appStore.selectedApp.title }}
             </span><br>
-            <span
-              text="primaryOp dark:primary xl"
-              font="light"
-              >تطبيق</span>
+            <span text="primaryOp dark:primary xl" font="light">تطبيق</span>
             <div flex="~">
               <!-- Loading -->
-              <div
-                v-if="loading"
-                flex="~"
-                justify="center"
-                m-t="30px"
-                w="122px"
-                h="41px"
-                rounded="lg"
-                bg="yellow"
-              >
+              <div v-if="loading" flex="~" justify="center" m-t="30px" w="122px" h="41px" rounded="lg" bg="yellow">
                 <AppStoreIconLoading w="5" h="5" m-l="2" text="primaryOp" />
               </div>
               <!-- Owned -->
-              <div
-                v-else-if="appStore.selectedApp.owned"
-                flex="~"
-                justify="center"
-                m-t="30px"
-                w="122px"
-                h="41px"
-                grid="~ flow-row"
-                class="place-items-center"
-                rounded="lg"
-                bg="green"
-              >
+              <div v-else-if="appStore.selectedApp.owned" flex="~" justify="center" m-t="30px" w="122px" h="41px" grid="~ flow-row" class="place-items-center" rounded="lg" bg="green">
                 <AppStoreIconCheck w="5" h="5" m-l="2" text="primaryOp" />
                 <span text="md primaryOp"> تم الشراء </span>
               </div>
               <!-- Not Owned -->
               <div v-else flex="~">
-                <div
-                  @click="toggleModal()"
-                  cursor="pointer"
-                  flex="~"
-                  justify="center"
-                  m-t="30px"
-                  w="122px"
-                  h="41px"
-                  grid="~ flow-row"
-                  class="place-items-center"
-                  rounded="lg"
-                  bg="primaryOp dark:primary"
-                >
-                  <AppStoreIconInstall
-                    w="5"
-                    h="5"
-                    m-l="2"
-                    text="primary dark:primaryOp"
-                  />
+                <div @click="toggleModal()" cursor="pointer" flex="~" justify="center" m-t="30px" w="122px" h="41px" grid="~ flow-row" class="place-items-center" rounded="lg" bg="primaryOp dark:primary">
+                  <AppStoreIconInstall w="5" h="5" m-l="2" text="primary dark:primaryOp" />
                   <span text="md primary dark:primaryOp" cursor="pointer">
                     {{ appStore.selectedApp.points > 0 ? "تنصيب" : "مجانا" }}
                   </span>
                 </div>
-                <span
-                  flex="~"
-                  text="md primaryOp dark:primary 2xl"
-                  m-t="9"
-                  m-r="3"
-                >
-                  <div class="i-ri:copper-coin-fill" w="32px" h="32px"></div>
+                <span flex="~" text="md primaryOp dark:primary 2xl" m-t="9" m-r="3">
+                  <AppStoreIconCoin w="32px" h="32px" m-l="2" />
                   {{
-                    appStore.selectedApp.points > 0
-                      ? appStore.selectedApp.points
-                      : "مجانا"
+                  appStore.selectedApp.points > 0
+                  ? appStore.selectedApp.points
+                  : "مجانا"
                   }}
                 </span>
               </div>
@@ -125,22 +60,18 @@
         </div>
 
         <!-- Content description -->
-        <div m-t="3"
-          :class="
-            !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
-              ? 'w-70%'
-              : 'w-full'
-          "
-        >
+        <div m-t="3" :class="
+          !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
+            ? 'w-70%'
+            : 'w-full'
+        ">
           <span text="primaryOp dark:secondaryOp 2xl">{{
-            appStore.selectedApp.description
+          appStore.selectedApp.description
           }}</span>
-          <span text="primaryOp dark:secondaryOp 2xl"
-            >Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+          <span text="primaryOp dark:secondaryOp 2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
             voluptates harum corporis amet alias temporibus id dignissimos quae
             molestiae. Esse odit nam ipsa debitis facilis cumque omnis odio
-            perferendis nostrum!</span
-          >
+            perferendis nostrum!</span>
         </div>
 
         <!-- Extended Services -->
@@ -150,65 +81,36 @@
             <span text="primaryOp dark:primary 2xl">
               خدمات اضافية داخل التطبيق
             </span>
-            <div
-              grid="~ flow-col"
-              :class="
-                !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
-                  ? 'w-70%'
-                  : 'w-100%'
-              "
-              h="auto"
-              class="overflow-x-scroll overflow-y-hidden"
-            >
-              <LazyAppStoreAppsServiceCard
-                v-for="service in appStore.selectedApp.getAllServices()"
-                :key="'service-' + service"
-                :service="service"
-              />
+            <div grid="~ flow-col" :class="
+              !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
+                ? 'w-70%'
+                : 'w-100%'
+            " h="auto" class="overflow-x-scroll overflow-y-hidden">
+              <LazyAppStoreAppsServiceCard v-for="service in appStore.selectedApp.getAllServices()" :key="'service-' + service" :service="service" />
             </div>
           </div>
           <div v-else>
-            <span text="primaryOp dark:primary 2xl"
-              >لا توجد خدمات لهذا التطبيق حاليا</span
-            >
+            <span text="primaryOp dark:primary 2xl">لا توجد خدمات لهذا التطبيق حاليا</span>
           </div>
         </div>
       </div>
       <!-- Left Section - Packs -  -->
-      <div
-        :class="
-            !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
-              ? 'w-30%'
-              : 'w-95%'
-          "
-        flex="~ col"
-        items="center">
-        <span text="priamryOp dark:primary" font="bold"
-          >التطبيق متوفر في الحزم التالية :</span
-        >
-        <AppStoreAppsPackCard
-          m-y="4"
-          v-for="pack in packs"
-          :key="'pack-' + pack.id"
-          :pack="pack"
-        />
-        <span text="priamryOp dark:primary" font="light"
-          >شراء الحزم يساعدك على توفير المال
+      <div :class="
+        !(breakpoint.twoXs || breakpoint.xs || breakpoint.sm || breakpoint.md)
+          ? 'w-30%'
+          : 'w-95%'
+      " flex="~ col" items="center">
+        <span text="priamryOp dark:primary" font="bold">التطبيق متوفر في الحزم التالية :</span>
+        <AppStoreAppsPackCard m-y="4" v-for="pack in packs" :key="'pack-' + pack.id" :pack="pack" />
+        <span text="priamryOp dark:primary" font="light">شراء الحزم يساعدك على توفير المال
         </span>
       </div>
     </div>
     <!-- Buy App Modal -->
     <Teleport to="body" w="950px" h="630px">
-      <UiModal
-        v-model="stateModal"
-        @cancel="modalCanceled"
-        align="center"
-      >
+      <UiModal v-model="stateModal" @cancel="modalCanceled" align="center">
         <template v-slot:title>أختيار الخطة</template>
-        <AppStorePlans
-          :app="appStore.selectedApp"
-          :confirm="buyApp"
-        />
+        <AppStorePlans :app="appStore.selectedApp" :confirm="buyApp" />
       </UiModal>
     </Teleport>
   </div>
@@ -255,4 +157,6 @@ window.onpopstate = function () {
 const breakpoint = appStore.getBreakpoints;
 </script>
 
-<style></style>
+<style>
+
+</style>
