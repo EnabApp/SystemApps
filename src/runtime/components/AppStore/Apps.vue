46d6cc983e49cf53b545d1d6a9be5cf75d0e1b6b
Montazer Malik <64214350+ste7een@users.noreply.github.com>
2022-09-24 01:03:06 +0300

    new

--- src/runtime/components/AppStore/Apps.vue
+++ src/runtime/components/AppStore/Apps.vue
@@ -2,14 +2,13 @@
   <div>
     <div m-t="3%" flex="~ col">
       <div v-if="!(breakpoint.twoXs || breakpoint.xs)" grid="~" place="items-center" :class="(breakpoint.md || breakpoint.sm ) ? 'grid-cols-4' : breakpoint.lg ? 'grid-cols-4' : breakpoint.xl ? 'grid-cols-6' : breakpoint.twoXl ? 'grid-cols-6' : 'grid-cols-8' ">
-        <AppStoreAppsPackCard v-for="pack in packs" :key="'pack-'+ pack.id " :pack="pack" />
+        <AppStoreAppsPackCard v-for="pack in appStore.getPacks" :key="'pack-'+ pack.id " :pack="pack" />
       </div>
       <div v-else class="max-w-90% mx-auto min-w-0">
         <div class="overflow-x-auto flex gap-2">
-          <AppStoreAppsPackCard v-for="pack in packs" :key="'pack-'+ pack.id " :pack="pack" />
+          <AppStoreAppsPackCard v-for="pack in appStore.getPacks" :key="'pack-'+ pack.id " :pack="pack" />
         </div>
       </div>
-      <span text="primary 2xl">{{appStore.allPacks2}}</span>
       <div m-t="6" grid="~ gap-2" place="items-center" :class="(breakpoint.twoXs || breakpoint.xs ) ? 'grid-cols-2' : (breakpoint.md || breakpoint.sm ) ? 'grid-cols-3' : breakpoint.lg ? 'grid-cols-4' : breakpoint.xl ? 'grid-cols-5' : breakpoint.twoXl ? 'grid-cols-6' : 'grid-cols-8' ">
         <AppStoreAppsCard v-for="card in appStore.appsWithoutCore" :key="'app-'+ card.id " :app="card" />
       </div>
@@ -24,6 +23,7 @@ const appStore = useAppStore()
 const appManager = useAppManager()
 const packs = computed(() => appStore.packs);
 const breakpoint = appStore.getBreakpoints
+
 </script>
 
 <style></style>
